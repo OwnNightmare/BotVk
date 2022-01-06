5724861deabd00f910d8b82fccbd3e660665e3cf
OwnNightmare <yura.borisov.9523@gmail.com>
2022-01-06 15:28:19 +0300

    classmethod

--- Vk/Vk_module.py
+++ Vk/Vk_module.py
@@ -5,10 +5,10 @@ import requests
 class VkClient:
 
     app_id = '8044074'
-    my_token_offline = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'
+    my_token = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'  # offline level
 
-    @staticmethod
-    def _open_page():
+    @classmethod
+    def open_page(cls):
         oauth_url = 'https://oauth.vk.com/authorize'
         params_open = "client_id=8044074&redirect_uri=https://oauth.vk.com/blank.html&scope=65538&display=page&response_type=token"
         webbrowser.open_new(f"{oauth_url}?{params_open}")
@@ -18,6 +18,6 @@ class VkClient:
 
 
 if __name__ == '__main__':
-    VkClient._open_page()
+    VkClient.open_page()
     user_token = input('Ваш Vk токен: ')
-    me = VkClient(VkClient.my_token_offline)
+    me = VkClient(VkClient.my_token)
