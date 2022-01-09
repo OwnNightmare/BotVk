16ae540cc11e2ba630b34cce06429c0f0d12e10f
OwnNightmare <yura.borisov.9523@gmail.com>
2022-01-09 18:58:28 +0300

    deleted my class

--- Tests/TestVk.py
+++ Tests/TestVk.py
@@ -1,12 +1,12 @@
 import pytest
 import unittest
-from Vk.VK_funcs import MyVkClass
+from Vk.VK_funcs import my_token
 
 
 
 class TestVkApi:
     """При инициализации класса создается объект класса VkClient - тестовый юзер с моим VK-токеном пользователя"""
-    test_user = MyVkClass(MyVkClass.my_token)
+
 
     def test_get_acc_info(self):
         """Тест получения информации о самом пользователе"""
--- Vk/VK_funcs.py
+++ Vk/VK_funcs.py
@@ -3,26 +3,26 @@ import datetime
 my_token = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'  # offline level
 
 
-class MyVkClass:
-
-    app_id = '8044074'
-    my_token = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'  # offline level
-    query_pattern = 'https://api.vk.com/method/METHOD?PARAMS&access_token=TOKEN&v=V'
-    url_methods = 'https://api.vk.com/method/'
-    bot_token = '3ed6d7a1af9a6f6789559a925b14b30963b1514d943c41926cb88b28ea1091dd321d9ddc494cfa694ba54'
-
-    def __init__(self, vk_token):
-        self.vk_token = vk_token
-        self.usual_params = {
-            'access_token': self.vk_token,
-            'v': '5.131'
-        }
-        self.photo_params = {'owner_id': '000000001',
-                             'album_id': 'profile',
-                             'extended': 1,
-                            'access_token': self.vk_token,
-                             'v': '5.131'}
-        self.portrait = {}
+# class MyVkClass:
+#
+#     app_id = '8044074'
+#     my_token = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'  # offline level
+#     query_pattern = 'https://api.vk.com/method/METHOD?PARAMS&access_token=TOKEN&v=V'
+#     url_methods = 'https://api.vk.com/method/'
+#     bot_token = '3ed6d7a1af9a6f6789559a925b14b30963b1514d943c41926cb88b28ea1091dd321d9ddc494cfa694ba54'
+#
+#     def __init__(self, vk_token):
+#         self.vk_token = vk_token
+#         self.usual_params = {
+#             'access_token': self.vk_token,
+#             'v': '5.131'
+#         }
+#         self.photo_params = {'owner_id': '000000001',
+#                              'album_id': 'profile',
+#                              'extended': 1,
+#                             'access_token': self.vk_token,
+#                              'v': '5.131'}
+#         self.portrait = {}
 
 
 def calc_age(acc_info: dict):
