d03d03b528eb7e8e9be78359e219320321e51c22
OwnNightmare <yura.borisov.9523@gmail.com>
2022-01-13 15:08:10 +0300

    start ignoring Vk/VK_funcs.py

--- Vk/VkBoting.py
+++ Vk/VkBoting.py
@@ -4,15 +4,12 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import time
 import datetime
 import sqlalchemy.engine.row
-import json
 from datetime import datetime
-from VK_funcs import my_token
 import json
 from random import shuffle
-# from vk_api.longpoll import VkLongPoll, VkEventType
 from DB import DataBase
 
-
+my_token = 'c3a240cff79d2ddac8a4e884df9b599090c3d54f166d62f5c2c3768d86a215fe590b7d62bc8a26a13ec15'  # offline level
 bot_token = '3ed6d7a1af9a6f6789559a925b14b30963b1514d943c41926cb88b28ea1091dd321d9ddc494cfa694ba54'  # ключ доступа бота
 group_id = 209978754  # ID вашего сообщества
 
